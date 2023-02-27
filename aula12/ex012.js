56810f07b00d11e1706ba3556b7b67b28c7df998
Patrik de Almeida <patrikalmeida1@hotmail.com>
2023-02-27 15:06:18 -0300

    postando com atraso, porém, o curso completo de JavaScript com o Professor Gustavo Guanabara.

---
+++ aula12/ex012.js
@@ -0,0 +1,31 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 5) {
+    console.log('Boa madrugada!');
+} else if (hora <= 12) {
+    console.log('Bom dia!');
+} else if (hora > 12 && hora < 18) {
+    console.log('Boa tarde!')
+} else if (hora >= 18) {
+    console.log('Boa noite!')
+} 
+
+
+/*
+
+EXERCÍCIO HORAS DO DIA
+
+ var hora = 19
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 5) {
+    console.log('Boa madrugada!');
+} else if (hora <= 12) {
+    console.log('Bom dia!');
+} else if (hora > 12 && hora < 18) {
+    console.log('Boa tarde!')
+} else if (hora >= 18) {
+    console.log('Boa noite!')
+} 
+
+*/
---
+++ aula13/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c + 1                               // ESSA OU A DE BAIXO, SÃO AS MELHORES!
+} while (c <= 6)
+
+/*
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)                          // A MAIS ENCONTRADA!
+    c++ // c + 1
+}
+
+*/
+
+
+
+
+/*
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')                  // A PIOR ESCOLHA! 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+*/
